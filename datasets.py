75a240d9273f53f6db34db192cbabdbe11af98d9
Sergio Evola <Evola.sergio97@gmail.com>
2022-07-15 12:01:23 +0200

    Updated datasets.txt to datasets.py

---
+++ datasets.py
@@ -0,0 +1,10 @@
+datasets_dictionary = {
+    'accomodation':'https://tourism.api.opendatahub.bz.it/v1/Accommodation/5CEA544EE34639034F07B79D4AEEB603_REDUCED?idsource=lts&availabilitychecklanguage=en&roominfo=1-18%2C18&bokfilter=hgv&source=sinfo&detail=0&removenullvalues=false',
+    'event':'https://tourism.api.opendatahub.bz.it/v1/Event/BFEB2DDB0FD54AC9BC040053A5514A92_REDUCED?removenullvalues=false',
+    'weather':'https://tourism.api.opendatahub.bz.it/v1/Weather?language=en&extended=true',
+    'webcam_info':'https://tourism.api.opendatahub.bz.it/v1/WebcamInfo?pagenumber=1&pagesize=1&removenullvalues=false',
+    'article':'https://tourism.api.opendatahub.bz.it/v1/Article?pagenumber=1&pagesize=1&articletype=255&removenullvalues=false',
+    'event_short':'https://tourism.api.opendatahub.bz.it/v1/EventShort?pagenumber=1&pagesize=1&sortorder=ASC&optimizedates=false&removenullvalues=false',
+    'ODH_activity_Poi':'https://tourism.api.opendatahub.bz.it/v1/ODHActivityPoi?pagenumber=1&pagesize=1&type=255&removenullvalues=false'
+}
+
